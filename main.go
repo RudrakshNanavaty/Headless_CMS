5c5b92272a9ea2e61d7aa5b9078c562ed6043aca
Saurav Navdhare <navdharesaurav2003@gmail.com>
2024-09-26 21:00:22 +0530

    Added initializers

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+	"headless-cms/initializers"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+}
 
 func main() {
-	fmt.Println("Hello World")
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+		})
+	})
+	r.Run()
 }
---
+++ types/types.go
@@ -0,0 +1,52 @@
+package types
+
+import "time"
+
+// Type table model
+type Type struct {
+	ID   uint   `json:"type_id" gorm:"primaryKey"`
+	Name string `json:"name" gorm:"not null; unique"`
+	// add a field with unique tag
+}
+
+// Content table model
+type Content struct {
+	ID         uint `json:"content_id" gorm:"primaryKey"`
+	TypeID     uint `json:"type_id" gorm:"not null"`
+	Type       Type `gorm:"foreignKey:TypeID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+	Data       Data        `json:"data" gorm:"foreignKey:ContentID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Attributes []Attribute `json:"attributes" gorm:"foreignKey:ContentID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Children   []*Content  `json:"children" gorm:"many2many:content_children;joinForeignKey:ParentID;joinReferences:ChildID"`
+}
+
+// Data table model
+type Data struct {
+	ID        uint     `json:"data_id" gorm:"primaryKey"`
+	ContentID uint     `json:"content_id" gorm:"not null"`
+	Text      []string `json:"text" gorm:"type:text[]"`
+	ImageUrls []string `json:"image" gorm:"type:text[]"`
+	PdfUrls   []string `json:"pdf" gorm:"type:text[]"`
+	Code      []string `json:"code" gorm:"type:text[]"`
+	CreatedAt time.Time
+	Content   *Content `gorm:"foreignKey:ContentID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"` // Foreign key back to content
+}
+
+// Attribute table model
+type Attribute struct {
+	ID        uint   `json:"attribute_id" gorm:"primaryKey"`
+	ContentID uint   `json:"content_id" gorm:"not null"`
+	Name      string `json:"name" gorm:"not null"`
+	Value     string `json:"value" gorm:"type:jsonb"`
+	CreatedAt time.Time
+	Content   *Content `gorm:"foreignKey:ContentID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"` // Foreign key back to content
+}
+
+// Child table model | Children relationship table (self-referencing Content)
+type Child struct {
+	ParentID uint     `gorm:"primaryKey"`
+	ChildID  uint     `gorm:"primaryKey"`
+	Parent   *Content `gorm:"foreignKey:ParentID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Child    *Content `gorm:"foreignKey:ChildID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+}
